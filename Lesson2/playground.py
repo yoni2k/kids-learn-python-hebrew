b92d69c25e31e8bd33438b055067e4cf1df4ed18
yoni2k <yoni.k81@gmail.com>
2019-07-17 11:51:51 +0300

    Playground lesson 2

---
+++ Lesson2/playground.py
@@ -0,0 +1,79 @@
+# Numbers
+print(2 + 3)
+
+# Strings
+print("2" + "3")
+
+var = -1000000
+print(var)
+
+var = 1000000
+print(var)
+
+char = 'a'
+print(char)
+
+bool1 = True
+bool2 = False
+
+print(bool1)
+print(bool2)
+
+# and or not
+print("====== and:")
+print(True and False)
+print(False and True)
+print(False and False)
+print(True and True)
+
+print("======= or:")
+print(True or False)
+print(False or True)
+print(False or False)
+print(True or True)
+
+print("More than 2:")
+print(1 + 4 + 3) # print(5 + 3) --> print(8) --> print
+print(True and True and False)
+print(False and True and True and True and True and True and True)
+print(True or False or False or False or False)
+print(False or False or False or False or True or False)
+def printNumFalse():
+    print("In printNumFalse")
+    return False
+print(printNumFalse() and printNumFalse() and printNumFalse())
+print(printNumFalse() or printNumFalse() or printNumFalse())
+
+print("\nnot: ")
+print(not True)
+print(not False)
+print(True and not False)
+
+print("\n==:")
+print(True == True)
+print(1 == 1)
+print("Aviad" == "Ido")
+print("Avaid" == "aviad")
+
+print("\n>, <, >=, <=, !=:")
+print(1 < 2)
+print(2 < 2)
+print(3 > 2)
+print(2 > 3)
+print(2 <= 2)
+print(2 >= 2)
+print(2 != 2)
+var1 = 1
+var2 = 2
+print(var1 != var2)
+print((var1 > var2) or (var1 < var2))
+print(not(var1 == var2))
+
+
+
+
+
+
+
+
+
